bfd86b2a64fd0d2d04d4da84e441d724656a7fb5
garapro <garapro@garage-programming.com>
2018-09-30 19:24:54 +0900

    プロジェクト作成 Harmony 設定

---
+++ firmware/src/pic_spi_temp.c
@@ -0,0 +1,395 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+  
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    pic_spi_temp.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It 
+    implements the logic of the application's state machine and it may call 
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files 
+// *****************************************************************************
+// *****************************************************************************
+
+#include "pic_spi_temp.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+    
+    Application strings and buffers are be defined outside this structure.
+*/
+
+PIC_SPI_TEMP_DATA pic_spi_tempData;
+/* Static buffers, suitable for DMA transfer */
+#define PIC_SPI_TEMP_MAKE_BUFFER_DMA_READY  __attribute__((coherent)) __attribute__((aligned(16)))
+
+static uint8_t PIC_SPI_TEMP_MAKE_BUFFER_DMA_READY writeBuffer[PIC_SPI_TEMP_USB_CDC_COM_PORT_SINGLE_WRITE_BUFFER_SIZE];
+static uint8_t writeString[] = "Hello World\r\n";
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/*******************************************************
+ * USB CDC Device Events - Application Event Handler
+ *******************************************************/
+
+USB_DEVICE_CDC_EVENT_RESPONSE PIC_SPI_TEMP_USBDeviceCDCEventHandler
+(
+    USB_DEVICE_CDC_INDEX index ,
+    USB_DEVICE_CDC_EVENT event ,
+    void * pData,
+    uintptr_t userData
+)
+{
+    PIC_SPI_TEMP_DATA * appDataObject;
+    appDataObject = (PIC_SPI_TEMP_DATA *)userData;
+    USB_CDC_CONTROL_LINE_STATE * controlLineStateData;
+
+    switch ( event )
+    {
+        case USB_DEVICE_CDC_EVENT_GET_LINE_CODING:
+
+            /* This means the host wants to know the current line
+             * coding. This is a control transfer request. Use the
+             * USB_DEVICE_ControlSend() function to send the data to
+             * host.  */
+
+            USB_DEVICE_ControlSend(appDataObject->deviceHandle,
+                    &appDataObject->getLineCodingData, sizeof(USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_LINE_CODING:
+
+            /* This means the host wants to set the line coding.
+             * This is a control transfer request. Use the
+             * USB_DEVICE_ControlReceive() function to receive the
+             * data from the host */
+
+            USB_DEVICE_ControlReceive(appDataObject->deviceHandle,
+                    &appDataObject->setLineCodingData, sizeof(USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_CONTROL_LINE_STATE:
+
+            /* This means the host is setting the control line state.
+             * Read the control line state. We will accept this request
+             * for now. */
+
+            controlLineStateData = (USB_CDC_CONTROL_LINE_STATE *)pData;
+            appDataObject->controlLineStateData.dtr = controlLineStateData->dtr;
+            appDataObject->controlLineStateData.carrier = controlLineStateData->carrier;
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SEND_BREAK:
+
+            /* This means that the host is requesting that a break of the
+             * specified duration be sent.  */
+            break;
+
+        case USB_DEVICE_CDC_EVENT_READ_COMPLETE:
+            /* This means that the host has sent some data*/
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_RECEIVED:
+
+            /* The data stage of the last control transfer is
+             * complete. For now we accept all the data */
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_SENT:
+
+            /* This means the GET LINE CODING function data is valid. We dont
+             * do much with this data in this demo. */
+            break;
+
+        case USB_DEVICE_CDC_EVENT_WRITE_COMPLETE:
+
+            /* This means that the host has sent some data*/
+            appDataObject->writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+            break;
+
+        default:
+            break;
+    }
+
+    return USB_DEVICE_CDC_EVENT_RESPONSE_NONE;
+}
+
+/***********************************************
+ * Application USB Device Layer Event Handler.
+ ***********************************************/
+void PIC_SPI_TEMP_USBDeviceEventHandler ( USB_DEVICE_EVENT event, void * eventData, uintptr_t context )
+{
+    USB_DEVICE_EVENT_DATA_CONFIGURED *configuredEventData;
+
+    switch ( event )
+    {
+        case USB_DEVICE_EVENT_SOF:
+            break;
+
+        case USB_DEVICE_EVENT_RESET:
+
+            pic_spi_tempData.isConfigured = false;
+
+            break;
+
+        case USB_DEVICE_EVENT_CONFIGURED:
+
+            /* Check the configuration. We only support configuration 1 */
+            configuredEventData = (USB_DEVICE_EVENT_DATA_CONFIGURED*)eventData;
+            if ( configuredEventData->configurationValue == 1)
+            {
+                /* Register the CDC Device application event handler here.
+                 * Note how the pic_spi_tempData object pointer is passed as the
+                 * user data */
+
+                USB_DEVICE_CDC_EventHandlerSet(USB_DEVICE_CDC_INDEX_0, PIC_SPI_TEMP_USBDeviceCDCEventHandler, (uintptr_t)&pic_spi_tempData);
+
+                /* Mark that the device is now configured */
+                pic_spi_tempData.isConfigured = true;
+
+            }
+            break;
+
+        case USB_DEVICE_EVENT_POWER_DETECTED:
+
+            /* VBUS was detected. We can attach the device */
+            USB_DEVICE_Attach(pic_spi_tempData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_POWER_REMOVED:
+
+            /* VBUS is not available any more. Detach the device. */
+            USB_DEVICE_Detach(pic_spi_tempData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_SUSPENDED:
+            break;
+
+        case USB_DEVICE_EVENT_RESUMED:
+        case USB_DEVICE_EVENT_ERROR:
+        default:
+            break;
+    }
+}
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/******************************************************************************
+  Function:
+    static void USB_TX_Task (void)
+    
+   Remarks:
+    Feeds the USB write function. 
+*/
+static void USB_TX_Task (void)
+{
+    if(!pic_spi_tempData.isConfigured)
+    {
+        pic_spi_tempData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+    }
+    else
+    {
+        /* Schedule a write if data is pending 
+         */
+        if ((pic_spi_tempData.writeLen > 0)/* && (pic_spi_tempData.writeTransferHandle == USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID)*/)
+        {
+            USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                                 &pic_spi_tempData.writeTransferHandle,
+                                 writeBuffer, 
+                                 pic_spi_tempData.writeLen,
+                                 USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+        }
+    }
+}
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void PIC_SPI_TEMP_Initialize ( void )
+
+  Remarks:
+    See prototype in pic_spi_temp.h.
+ */
+
+void PIC_SPI_TEMP_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    pic_spi_tempData.state = PIC_SPI_TEMP_STATE_INIT;
+
+
+    /* Device Layer Handle  */
+    pic_spi_tempData.deviceHandle = USB_DEVICE_HANDLE_INVALID ;
+
+    /* Device configured status */
+    pic_spi_tempData.isConfigured = false;
+
+    /* Initial get line coding state */
+    pic_spi_tempData.getLineCodingData.dwDTERate   = 9600;
+    pic_spi_tempData.getLineCodingData.bParityType =  0;
+    pic_spi_tempData.getLineCodingData.bParityType = 0;
+    pic_spi_tempData.getLineCodingData.bDataBits   = 8;
+
+
+    /* Write Transfer Handle */
+    pic_spi_tempData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+    
+    /*Initialize the write data */
+    pic_spi_tempData.writeLen = sizeof(writeString);
+	memcpy(writeBuffer, writeString, pic_spi_tempData.writeLen);
+    
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */
+}
+
+
+/******************************************************************************
+  Function:
+    void PIC_SPI_TEMP_Tasks ( void )
+
+  Remarks:
+    See prototype in pic_spi_temp.h.
+ */
+
+void PIC_SPI_TEMP_Tasks ( void )
+{
+
+    /* Check the application's current state. */
+    switch ( pic_spi_tempData.state )
+    {
+        /* Application's initial state. */
+        case PIC_SPI_TEMP_STATE_INIT:
+        {
+            bool appInitialized = true;
+       
+
+            /* Open the device layer */
+            if (pic_spi_tempData.deviceHandle == USB_DEVICE_HANDLE_INVALID)
+            {
+                pic_spi_tempData.deviceHandle = USB_DEVICE_Open( USB_DEVICE_INDEX_0,
+                                               DRV_IO_INTENT_READWRITE );
+                appInitialized &= ( USB_DEVICE_HANDLE_INVALID != pic_spi_tempData.deviceHandle );
+            }
+        
+            if (appInitialized)
+            {
+
+                /* Register a callback with device layer to get event notification (for end point 0) */
+                USB_DEVICE_EventHandlerSet(pic_spi_tempData.deviceHandle,
+                                           PIC_SPI_TEMP_USBDeviceEventHandler, 0);
+            
+                pic_spi_tempData.state = PIC_SPI_TEMP_STATE_SERVICE_TASKS;
+            }
+            break;
+        }
+
+        case PIC_SPI_TEMP_STATE_SERVICE_TASKS:
+        {
+            USB_TX_Task();
+        
+            break;
+        }
+
+        /* TODO: implement your application state machine.*/
+        
+
+        /* The default state should never be executed. */
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
+
+ 
+
+/*******************************************************************************
+ End of File
+ */
---
+++ firmware/src/system_config/default/system_config.h
@@ -0,0 +1,315 @@
+/*******************************************************************************
+  MPLAB Harmony System Configuration Header
+
+  File Name:
+    system_config.h
+
+  Summary:
+    Build-time configuration header for the system defined by this MPLAB Harmony
+    project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    This configuration header must not define any prototypes or data
+    definitions (or include any files that do).  It only provides macro
+    definitions for build-time configuration options that are not instantiated
+    until used by another MPLAB Harmony module or application.
+
+    Created with MPLAB Harmony Version 2.06
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef _SYSTEM_CONFIG_H
+#define _SYSTEM_CONFIG_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/*  This section Includes other configuration headers necessary to completely
+    define this configuration.
+*/
+
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Service Configuration
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+/* Common System Service Configuration Options
+*/
+#define SYS_VERSION_STR           "2.06"
+#define SYS_VERSION               20600
+
+// *****************************************************************************
+/* Clock System Service Configuration Options
+*/
+#define SYS_CLK_FREQ                        200000000ul
+#define SYS_CLK_BUS_PERIPHERAL_1            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_2            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_3            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_4            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_5            100000000ul
+#define SYS_CLK_BUS_PERIPHERAL_7            200000000ul
+#define SYS_CLK_BUS_PERIPHERAL_8            100000000ul
+#define SYS_CLK_CONFIG_PRIMARY_XTAL         24000000ul
+#define SYS_CLK_CONFIG_SECONDARY_XTAL       32768ul
+   
+/*** Ports System Service Configuration ***/
+#define SYS_PORT_B_ANSEL        0x7DFF
+#define SYS_PORT_B_TRIS         0x7FFF
+#define SYS_PORT_B_LAT          0x0000
+#define SYS_PORT_B_ODC          0x0000
+#define SYS_PORT_B_CNPU         0x0000
+#define SYS_PORT_B_CNPD         0x0000
+#define SYS_PORT_B_CNEN         0x0000
+
+#define SYS_PORT_D_ANSEL        0x3FFD
+#define SYS_PORT_D_TRIS         0xFFFF
+#define SYS_PORT_D_LAT          0x0000
+#define SYS_PORT_D_ODC          0x0000
+#define SYS_PORT_D_CNPU         0x0000
+#define SYS_PORT_D_CNPD         0x0000
+#define SYS_PORT_D_CNEN         0x0000
+
+#define SYS_PORT_H_ANSEL        0xFEFF
+#define SYS_PORT_H_TRIS         0xFFFF
+#define SYS_PORT_H_LAT          0x0000
+#define SYS_PORT_H_ODC          0x0000
+#define SYS_PORT_H_CNPU         0x0000
+#define SYS_PORT_H_CNPD         0x0000
+#define SYS_PORT_H_CNEN         0x0000
+
+
+/*** Interrupt System Service Configuration ***/
+#define SYS_INT                     true
+/*** Timer System Service Configuration ***/
+#define SYS_TMR_POWER_STATE             SYS_MODULE_POWER_RUN_FULL
+#define SYS_TMR_DRIVER_INDEX            DRV_TMR_INDEX_0
+#define SYS_TMR_MAX_CLIENT_OBJECTS      5
+#define SYS_TMR_FREQUENCY               1000
+#define SYS_TMR_FREQUENCY_TOLERANCE     10
+#define SYS_TMR_UNIT_RESOLUTION         10000
+#define SYS_TMR_CLIENT_TOLERANCE        10
+#define SYS_TMR_INTERRUPT_NOTIFICATION  true
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+/*** SPI Driver Configuration ***/
+#define DRV_SPI_NUMBER_OF_MODULES		6
+/*** Driver Compilation and static configuration options. ***/
+/*** Select SPI compilation units.***/
+#define DRV_SPI_POLLED 				0
+#define DRV_SPI_ISR 				1
+#define DRV_SPI_MASTER 				1
+#define DRV_SPI_SLAVE 				0
+#define DRV_SPI_RM 					0
+#define DRV_SPI_EBM 				1
+#define DRV_SPI_8BIT 				1
+#define DRV_SPI_16BIT 				0
+#define DRV_SPI_32BIT 				0
+#define DRV_SPI_DMA 				0
+
+/*** SPI Driver Static Allocation Options ***/
+#define DRV_SPI_INSTANCES_NUMBER 		1
+#define DRV_SPI_CLIENTS_NUMBER 			1
+#define DRV_SPI_ELEMENTS_PER_QUEUE 		10
+/* SPI Driver Instance 0 Configuration */
+#define DRV_SPI_SPI_ID_IDX0 				SPI_ID_1
+#define DRV_SPI_TASK_MODE_IDX0 				DRV_SPI_TASK_MODE_ISR
+#define DRV_SPI_SPI_MODE_IDX0				DRV_SPI_MODE_MASTER
+#define DRV_SPI_ALLOW_IDLE_RUN_IDX0			false
+#define DRV_SPI_SPI_PROTOCOL_TYPE_IDX0 		DRV_SPI_PROTOCOL_TYPE_STANDARD
+#define DRV_SPI_COMM_WIDTH_IDX0 			SPI_COMMUNICATION_WIDTH_8BITS
+#define DRV_SPI_CLOCK_SOURCE_IDX0 		    SPI_BAUD_RATE_PBCLK_CLOCK
+#define DRV_SPI_SPI_CLOCK_IDX0 				CLK_BUS_PERIPHERAL_2
+#define DRV_SPI_BAUD_RATE_IDX0 				1000000
+#define DRV_SPI_BUFFER_TYPE_IDX0 			DRV_SPI_BUFFER_TYPE_ENHANCED
+#define DRV_SPI_CLOCK_MODE_IDX0 			DRV_SPI_CLOCK_MODE_IDLE_HIGH_EDGE_RISE
+#define DRV_SPI_INPUT_PHASE_IDX0 			SPI_INPUT_SAMPLING_PHASE_IN_MIDDLE
+#define DRV_SPI_TRANSMIT_DUMMY_BYTE_VALUE_IDX0      0xFF
+
+#define DRV_SPI_TX_INT_SOURCE_IDX0 			INT_SOURCE_SPI_1_TRANSMIT
+#define DRV_SPI_RX_INT_SOURCE_IDX0 			INT_SOURCE_SPI_1_RECEIVE
+#define DRV_SPI_ERROR_INT_SOURCE_IDX0 		INT_SOURCE_SPI_1_ERROR
+#define DRV_SPI_TX_INT_VECTOR_IDX0			INT_VECTOR_SPI1_TX
+#define DRV_SPI_RX_INT_VECTOR_IDX0			INT_VECTOR_SPI1_RX
+#define DRV_DRV_SPI_ERROR_INT_VECTOR_IDX0	INT_VECTOR_SPI1_FAULT
+#define DRV_SPI_TX_INT_PRIORITY_IDX0 		INT_PRIORITY_LEVEL1
+#define DRV_SPI_TX_INT_SUB_PRIORITY_IDX0 	INT_SUBPRIORITY_LEVEL0
+#define DRV_SPI_RX_INT_PRIORITY_IDX0 		INT_PRIORITY_LEVEL1
+#define DRV_SPI_RX_INT_SUB_PRIORITY_IDX0 	INT_SUBPRIORITY_LEVEL0
+#define DRV_SPI_ERROR_INT_PRIORITY_IDX0 	INT_PRIORITY_LEVEL1
+#define DRV_SPI_ERROR_INT_SUB_PRIORITY_IDX0 INT_SUBPRIORITY_LEVEL0
+#define DRV_SPI_QUEUE_SIZE_IDX0 			10
+#define DRV_SPI_RESERVED_JOB_IDX0 			1
+/*** Timer Driver Configuration ***/
+#define DRV_TMR_INTERRUPT_MODE             true
+#define DRV_TMR_INSTANCES_NUMBER           1
+#define DRV_TMR_CLIENTS_NUMBER             1
+
+/*** Timer Driver 0 Configuration ***/
+#define DRV_TMR_PERIPHERAL_ID_IDX0          TMR_ID_1
+#define DRV_TMR_INTERRUPT_SOURCE_IDX0       INT_SOURCE_TIMER_1
+#define DRV_TMR_INTERRUPT_VECTOR_IDX0       INT_VECTOR_T1
+#define DRV_TMR_ISR_VECTOR_IDX0             _TIMER_1_VECTOR
+#define DRV_TMR_INTERRUPT_PRIORITY_IDX0     INT_PRIORITY_LEVEL1
+#define DRV_TMR_INTERRUPT_SUB_PRIORITY_IDX0 INT_SUBPRIORITY_LEVEL0
+#define DRV_TMR_CLOCK_SOURCE_IDX0           DRV_TMR_CLKSOURCE_INTERNAL
+#define DRV_TMR_PRESCALE_IDX0               TMR_PRESCALE_VALUE_256
+#define DRV_TMR_OPERATION_MODE_IDX0         DRV_TMR_OPERATION_MODE_16_BIT
+#define DRV_TMR_ASYNC_WRITE_ENABLE_IDX0     false
+#define DRV_TMR_POWER_STATE_IDX0            SYS_MODULE_POWER_RUN_FULL
+
+
+ 
+// *****************************************************************************
+// *****************************************************************************
+// Section: Middleware & Other Library Configuration
+// *****************************************************************************
+// *****************************************************************************
+
+/*** USB Driver Configuration ***/
+
+
+/* Enables Device Support */
+#define DRV_USBHS_DEVICE_SUPPORT      true
+
+/* Disable Host Support */
+#define DRV_USBHS_HOST_SUPPORT      false
+
+/* Maximum USB driver instances */
+#define DRV_USBHS_INSTANCES_NUMBER    1
+
+/* Interrupt mode enabled */
+#define DRV_USBHS_INTERRUPT_MODE      true
+
+
+/* Number of Endpoints used */
+#define DRV_USBHS_ENDPOINTS_NUMBER    3
+
+
+
+
+/*** USB Device Stack Configuration ***/
+
+
+
+
+
+
+
+
+
+
+/* The USB Device Layer will not initialize the USB Driver */
+#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
+
+/* Maximum device layer instances */
+#define USB_DEVICE_INSTANCES_NUMBER     1
+
+/* EP0 size in bytes */
+#define USB_DEVICE_EP0_BUFFER_SIZE      64
+
+/* Enable SOF Events */ 
+#define USB_DEVICE_SOF_EVENT_ENABLE     
+
+
+
+
+
+
+
+
+
+/* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER     1
+
+
+
+
+
+
+
+
+
+
+/* CDC Transfer Queue Size for both read and
+   write. Applicable to all instances of the
+   function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED 3
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Configuration
+// *****************************************************************************
+// *****************************************************************************
+/*** Application Defined Pins ***/
+
+/*** Functions for CS pin ***/
+#define CSToggle() PLIB_PORTS_PinToggle(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15)
+#define CSOn() PLIB_PORTS_PinSet(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15)
+#define CSOff() PLIB_PORTS_PinClear(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15)
+#define CSStateGet() PLIB_PORTS_PinGetLatched(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15)
+#define CSStateSet(Value) PLIB_PORTS_PinWrite(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15, Value)
+
+
+/*** Application Instance 0 Configuration ***/
+
+#define PIC_SPI_TEMP_USB_CDC_COM_PORT_SINGLE_WRITE_BUFFER_SIZE 512
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // _SYSTEM_CONFIG_H
+/*******************************************************************************
+ End of File
+*/
